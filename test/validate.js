c65146d6149100d4f1a3a9741d6c4a788e33ae5e
vivsriaus <visriniv@microsoft.com>
2017-10-16 18:03:54 -0700

    Initial commit

---
+++ test/validate.js
@@ -0,0 +1,34 @@
+/*global describe, it, beforeEach*/
+var assert = require('assert');
+
+const filesFolder = './';
+var fs = require('fs');
+var path = require('path');
+var createUiDefFiles = [];
+
+var read = require('fs-readdir-recursive');
+var should = require('should');
+
+var createUiDefFileName = "createuidefinition.json";
+
+// Recursively get all files
+var files = read(filesFolder, function(x) {
+    return x !== 'node_modules' && x !== '.git' && x !== '.stripped';
+});
+
+// Get only create ui def files
+var createUiDefFiles = files.filter(function(file) {
+    return file.toLowerCase().indexOf(createUiDefFileName) !== -1;
+});
+
+describe('createUiDef tests', () => {
+    it('must have a schema property', () => {
+        createUiDefFiles.forEach(f => {
+            var fileString = fs.readFileSync(f, {
+                encoding: 'utf8'
+            }).trim();
+            var fileJSONObject = JSON.parse(fileString);
+            fileJSONObject.should.have.property('$schema');
+        });
+    });
+});
