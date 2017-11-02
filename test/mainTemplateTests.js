7e096b51d4e5ce843abc82a733d8e2b44bc2da63
vivsriaus <visriniv@microsoft.com>
2017-11-01 17:06:23 -0700

    Run tests for all template files in folder, not just mainTemplate. also fix bugs

--- test/mainTemplateTests.js
+++ test/mainTemplateTests.js
@@ -2,10 +2,10 @@ var assert = require('assert');
 var util = require('./util');
 const filesFolder = './';
 var path = require('path');
-var chai = require('chai');  
-var assert = chai.assert;    // Using Assert style
-var expect = chai.expect;    // Using Expect style
-var should = chai.should();  // Using Should style
+var chai = require('chai');
+var assert = chai.assert; // Using Assert style
+var expect = chai.expect; // Using Expect style
+var should = chai.should(); // Using Should style
 
 var folder = process.env.npm_config_folder || filesFolder;
 
@@ -13,15 +13,17 @@ var mainTemplateFileJSONObject = util.getMainTemplateFile(folder).jsonObject;
 var mainTemplateFile = util.getMainTemplateFile(folder).file;
 var createUiDefFileJSONObject = util.getCreateUiDefFile(folder).jsonObject;
 var createUiDefFile = util.getCreateUiDefFile(folder).file;
+var templateFiles = util.getTemplateFiles(folder).files;
+var templateFileJSONObjects = util.getTemplateFiles(folder).fileJSONObjects;
 
-chai.use(function (_chai, _) {
-  _chai.Assertion.addMethod('withMessage', function (msg) {
-    _.flag(this, 'message', msg);
-  });
+chai.use(function(_chai, _) {
+    _chai.Assertion.addMethod('withMessage', function(msg) {
+        _.flag(this, 'message', msg);
+    });
 });
 
-function getErrorMessage(obj, message) {
-    return 'json object with \'name\' at line number ' + util.getPosition(obj, mainTemplateFile) + message;
+function getErrorMessage(obj, file, message) {
+    return 'json object with \'name\' at line number ' + util.getPosition(obj, file) + ' ' + message;
 }
 
 describe('mainTemplate tests', () => {
@@ -38,92 +40,115 @@ describe('mainTemplate tests', () => {
             var outputsInCreateUiDef = Object.keys(createUiDefJSONObject.parameters.outputs);
 
             // validate each parameter in main template has a value in outputs
-            mainTemplateFileJSONObject.should.have.property('parameters');
-            var parametersInMainTemplate = Object.keys(mainTemplateFileJSONObject.parameters);
-            parametersInMainTemplate.forEach(parameter => {
-                if(!mainTemplateFileJSONObject.parameters[parameter].defaultValue) {
-                    outputsInCreateUiDef.should.withMessage('outputs in createUiDefinition is missing the parameter ' + parameter).contain(parameter);
-                }
+            templateFileJSONObjects.forEach(templateJSONObject => {
+                var templateObject = templateJSONObject.value;
+                templateObject.should.have.property('parameters');
+                var parametersInMainTemplate = Object.keys(templateObject.parameters);
+                parametersInMainTemplate.forEach(parameter => {
+                    if (!templateObject.parameters[parameter].defaultValue) {
+                        outputsInCreateUiDef.should.withMessage('in file:' + templateJSONObject.filename + '. outputs in createUiDefinition is missing the parameter ' + parameter).contain(parameter);
+                    }
+                });
             });
         });
-        
+
         // TODO: should it be non null, or should all secure strings not contain default value?
         it('non-null default values must not be provided for secureStrings', () => {
-            Object.keys(mainTemplateFileJSONObject.parameters).forEach(parameter => {
-                if(mainTemplateFileJSONObject.parameters[parameter].type.toLowerCase()=='securestring') {
-                    // get default value if one exists
-                    var defaultVal = mainTemplateFileJSONObject.parameters[parameter].defaultValue;
-                    if(defaultVal && defaultVal.length > 0) {
-                        expect(mainTemplateFileJSONObject.parameters[parameter], parameter + ' should not have defaultValue').to.not.have.property('defaultValue');
+            templateFileJSONObjects.forEach(templateJSONObject => {
+                var templateObject = templateJSONObject.value;
+                Object.keys(templateObject.parameters).forEach(parameter => {
+                    if (templateObject.parameters[parameter].type.toLowerCase() == 'securestring') {
+                        // get default value if one exists
+                        var defaultVal = templateObject.parameters[parameter].defaultValue;
+                        if (defaultVal && defaultVal.length > 0) {
+                            expect(templateObject.parameters[parameter], 'in file:' + templateJSONObject.filename + parameter + ' should not have defaultValue').to.not.have.property('defaultValue');
+                        }
                     }
-                }
+                });
             });
+
         });
 
         // TODO: should location prop in resources all be set to param('location') ?
         it('a parameter named "location" must exists and must be used on all resource "location" properties', () => {
-            mainTemplateFileJSONObject.should.have.property('parameters');
-            mainTemplateFileJSONObject.parameters.should.have.property('location');
+            templateFileJSONObjects.forEach(templateJSONObject => {
+                var templateObject = templateJSONObject.value;
+                templateObject.should.have.property('parameters');
+                templateObject.parameters.should.have.property('location');
 
-            // TODO: if location default value exists, it should be set to resourceGroup.location(). Correct?
+                // TODO: if location default value exists, it should be set to resourceGroup.location(). Correct?
 
-            // each resource location should be "location": "[parameters('location')]"
-            var expectedLocation = '[parameters(\'location\')]';
-            var message = ' should have location set to [parameters(\'location\')]';
-            Object.keys(mainTemplateFileJSONObject.resources).forEach(resource => {
-                var val = mainTemplateFileJSONObject.resources[resource];
-                if(val.location) {
-                    val.location.should.withMessage(getErrorMessage(val, message)).be.eql(expectedLocation);
-                }
+                // each resource location should be "location": "[parameters('location')]"
+                var expectedLocation = '[parameters(\'location\')]';
+                var message = 'in file:' + templateJSONObject.filename + ' should have location set to [parameters(\'location\')]';
+                Object.keys(templateObject.resources).forEach(resource => {
+                    var val = templateObject.resources[resource];
+                    if (val.location) {
+                        val.location.split(' ').join('').should.withMessage(getErrorMessage(val, templateJSONObject.filepath, message)).be.eql(expectedLocation);
+                    }
+                });
             });
         });
 
         it('the template must not contain any unused parameters', () => {
-            mainTemplateFileJSONObject.should.have.property('parameters');
-            var parametersInMainTemplate = Object.keys(mainTemplateFileJSONObject.parameters);
-            parametersInMainTemplate.forEach(parameter => {
-                var paramString = 'parameters(\'' + parameter + '\')';
-                JSON.stringify(mainTemplateFileJSONObject).should.withMessage('unused parameter ' + parameter + ' in mainTemplate').contain(paramString);
+            templateFileJSONObjects.forEach(templateJSONObject => {
+                var templateObject = templateJSONObject.value;
+                templateObject.should.have.property('parameters');
+                var parametersInMainTemplate = Object.keys(templateObject.parameters);
+                parametersInMainTemplate.forEach(parameter => {
+                    var paramString = 'parameters(\'' + parameter + '\')';
+                    JSON.stringify(templateObject).should.withMessage('file:' + templateJSONObject.filename + '. unused parameter ' + parameter + ' in mainTemplate').contain(paramString);
+                });
             });
         });
     });
 
     describe('mainTemplate resources tests', () => {
         it('resourceGroup().location must not be used in the solution template except as a defaultValue for the location parameter', () => {
-            mainTemplateFileJSONObject.should.have.property('resources');
-            var locationString = '[resourceGroup().location]';
-            var message = ' should NOT have location set to [resourceGroup().location]';
-            Object.keys(mainTemplateFileJSONObject.resources).forEach(resource => {
-                var val = mainTemplateFileJSONObject.resources[resource];
-                if(val.location) {
-                    val.location.should.withMessage(getErrorMessage(val, message)).not.be.eql(locationString);
-                }
+            templateFileJSONObjects.forEach(templateJSONObject => {
+                var templateObject = templateJSONObject.value;
+                templateObject.should.have.property('resources');
+                var locationString = '[resourceGroup().location]';
+                var message = 'in file:' + templateJSONObject.filename + ' should NOT have location set to [resourceGroup().location]';
+                Object.keys(templateObject.resources).forEach(resource => {
+                    var val = templateObject.resources[resource];
+                    if (val.location) {
+                        val.location.should.withMessage(getErrorMessage(val, templateJSONObject.filepath, message)).not.be.eql(locationString);
+                    }
+                });
             });
         });
 
         it('apiVersions must NOT be retrieved using providers().apiVersions[n].  This function is non-deterministic', () => {
-            mainTemplateFileJSONObject.should.have.property('resources');
-            var message = ' should NOT have api version determined by providers().';
-            Object.keys(mainTemplateFileJSONObject.resources).forEach(resource => {
-                var val = mainTemplateFileJSONObject.resources[resource];
-                if(val.apiVersion) {
-                    val.apiVersion.should.withMessage(getErrorMessage(val, message)).not.contain('providers(');
-                }
+            templateFileJSONObjects.forEach(templateJSONObject => {
+                var templateObject = templateJSONObject.value;
+                templateObject.should.have.property('resources');
+                var message = 'in file:' + templateJSONObject.filename + ' should NOT have api version determined by providers().';
+                Object.keys(templateObject.resources).forEach(resource => {
+                    var val = templateObject.resources[resource];
+                    if (val.apiVersion) {
+                        val.apiVersion.should.withMessage(getErrorMessage(val, templateJSONObject.filepath, message)).not.contain('providers(');
+                    }
+                });
             });
         });
 
         it('template must contain only approved resources (Compute/Network/Storage)', () => {
-            mainTemplateFileJSONObject.should.have.property('resources');
-            var message = ' is NOT a compute, network or a storage resource type';
-            Object.keys(mainTemplateFileJSONObject.resources).forEach(resource => {
-                var val = mainTemplateFileJSONObject.resources[resource];
-                if(val.type) {
-                    var rType = val.type.split('/');
-                    var cond = rType[0].toLowerCase() == 'microsoft.compute' ||
-                        rType[0].toLowerCase() == 'microsoft.storage' ||
-                        rType[0].toLowerCase() == 'microsoft.network';
-                    expect(cond, getErrorMessage(val, message)).to.be.true;
-                }
+            templateFileJSONObjects.forEach(templateJSONObject => {
+                var templateObject = templateJSONObject.value;
+                templateObject.should.have.property('resources');
+                var message = 'in file:' + templateJSONObject.filename + ' is NOT a compute, network or a storage resource type';
+                Object.keys(templateObject.resources).forEach(resource => {
+                    var val = templateObject.resources[resource];
+                    if (val.type) {
+                        var rType = val.type.split('/');
+                        var cond = rType[0].toLowerCase() == 'microsoft.compute' ||
+                            rType[0].toLowerCase() == 'microsoft.storage' ||
+                            rType[0].toLowerCase() == 'microsoft.network' ||
+                            rType[0].toLowerCase() == 'microsoft.resources';
+                        expect(cond, getErrorMessage(val, templateJSONObject.filepath, message)).to.be.true;
+                    }
+                });
             });
         });
     });
--- test/util.js
+++ test/util.js
@@ -1,10 +1,10 @@
 var fs = require('fs');
 var lineColumn = require("line-column");
 var path = require('path');
-var chai = require('chai');  
-var assert = chai.assert;    // Using Assert style
-var expect = chai.expect;    // Using Expect style
-var should = chai.should();  // Using Should style
+var chai = require('chai');
+var assert = chai.assert; // Using Assert style
+var expect = chai.expect; // Using Expect style
+var should = chai.should(); // Using Should style
 var _this = this;
 
 var createUiDefFileName = "createuidefinition.json";
@@ -14,9 +14,13 @@ function getFiles(folder, fileType) {
     if (folder == './') {
         folder = __dirname + '/../';
     }
-    return fs.readdirSync(folder).filter(function(file) {
-        return file.toLowerCase().indexOf(fileType) !== -1;
-    });
+    if (fileType) {
+        return fs.readdirSync(folder).filter(function(file) {
+            return file.toLowerCase().indexOf(fileType) !== -1;
+        });
+    } else {
+        return fs.readdirSync(folder);
+    }
 }
 
 // Get create ui def file
@@ -24,8 +28,8 @@ exports.getCreateUiDefFile = function getCreateUiDefFile(folder) {
     var createUiDefFiles = getFiles(folder, createUiDefFileName);
     createUiDefFiles.length.should.equals(1, 'Only one createUiDefinition.json file should exist, but found ' + createUiDefFiles.length + ' file(s) in path ' + folder);
     var fileString = fs.readFileSync(path.resolve(folder, createUiDefFiles[0]), {
-            encoding: 'utf8'
-        }).trim();
+        encoding: 'utf8'
+    }).trim();
     return {
         file: path.resolve(folder, createUiDefFiles[0]),
         jsonObject: JSON.parse(fileString)
@@ -37,11 +41,38 @@ exports.getMainTemplateFile = function getMainTemplateFile(folder) {
     var mainTemplateFiles = getFiles(folder, mainTemplateFileName);
     mainTemplateFiles.length.should.equals(1, 'Only one mainTemplate.json file should exist, but found ' + mainTemplateFiles.length + ' file(s)');
     var fileString = fs.readFileSync(path.resolve(folder, mainTemplateFiles[0]), {
-            encoding: 'utf8'
-        }).trim();
+        encoding: 'utf8'
+    }).trim();
     return {
         file: path.resolve(folder, mainTemplateFiles[0]),
         jsonObject: JSON.parse(fileString)
+    };
+};
+
+// Get template files
+exports.getTemplateFiles = function getTemplateFiles(folder) {
+    var templateFiles = getFiles(folder, '.json');
+    var files = [];
+    var fileJSONObjects = [];
+    templateFiles.forEach(f => {
+        if (f.toLowerCase().indexOf(createUiDefFileName) == -1) {
+            var fileString = fs.readFileSync(path.resolve(folder, f), {
+                encoding: 'utf8'
+            }).trim();
+            var jsonObject = JSON.parse(fileString);
+            if (jsonObject.$schema && jsonObject.$schema.match('schema.management.azure.com/schemas/(.*)/deploymentTemplate.json')) {
+                files.push(path.resolve(folder, f));
+                fileJSONObjects.push({
+                    filename: f,
+                    value: jsonObject,
+                    filepath: path.resolve(folder, f)
+                });
+            }
+        }
+    });
+    return {
+        files: files,
+        fileJSONObjects: fileJSONObjects
     };
 };
 
@@ -58,19 +89,19 @@ exports.assertCreateUiDefExists = function assertCreateUiDefExists(folder) {
 // for now, get position of name element in file, so better error message can be thrown
 exports.getPosition = function getPosition(obj, file) {
     var fileString = fs.readFileSync(file, {
-            encoding: 'utf8'
-        }).trim();
+        encoding: 'utf8'
+    }).trim();
 
     // get the object "name"
     var stringToSearch;
     for (var key in obj) {
-        if(key.toLowerCase() == 'name') {
+        if (key.toLowerCase() == 'name') {
             stringToSearch = '"name": "' + obj[key] + '"';
         }
     }
     var ind = fileString.indexOf(stringToSearch);
     var lc = lineColumn(fileString).fromIndex(ind);
-    if(lc) {
+    if (lc) {
         return lc.line
     }
     return 'UNIDENTIFIED';
